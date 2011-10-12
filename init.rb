a4cfb2d77cc65891d5b503276ca97c481644a399
Elvuel <elvuel@gmail.com>
2011-10-12 17:30:44 +0800

    add customize middleware for test

--- init.rb
+++ init.rb
@@ -19,4 +19,26 @@ case ENV.fetch("RACK_ENV")
     Bundler.require :development
 end
 
+module RackOauth2
+  module Custom
+    class Serve < Struct.new :app, :options
+      def call(env)
+        request = Rack::Request.new(env)
+        params = request.params
+        if request.post?  and request.path == "/oauth/access_token" and params["client_id"] and params["grant_type"] == "password"
+          bundled_app = BundledApp.first(client_id: params["client_id"])
+          if bundled_app
+            app.call(env)
+          else # return or set params[:client_id] to nil
+            response = { error: 'unbundled_client', error_description: 'this client is not in bundle apps' }
+            return [403, { "Content-Type"=>"application/json", "Cache-Control"=>"no-store" }, [response.to_json]]
+          end
+        else
+          app.call(env)
+        end
+      end #call
+    end # Serve
+  end # Custom
+end # RackOauth2
+
 DataMapper.auto_upgrade!
