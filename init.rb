97a731e7037f98cc6cd985a0bed11909eb5a4032
Elvuel <elvuel@gmail.com>
2011-10-12 11:30:13 +0800

    add model BundledApp

--- init.rb
+++ init.rb
@@ -7,7 +7,9 @@ require_relative 'config'
 
 DataMapper.setup(:default, SQL_URL)
 
+# TODO refactor with Dir.glob(..).each { |file| require file } if many
 require_relative "models/user"
+require_relative "models/bundled_app"
 
 case ENV.fetch("RACK_ENV")
   when "test"
@@ -16,3 +18,5 @@ case ENV.fetch("RACK_ENV")
   when "development"
     Bundler.require :development
 end
+
+DataMapper.auto_upgrade!
---
+++ models/bundled_app.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+class BundledApp
+  include DataMapper::Resource
+  storage_names[:default] = 'bundled_apps'
+
+  property :id, Serial
+  property :name, String, required: true
+  property :client_id, String, required: true, unique: true
+  property :created_at, DateTime
+
+end
